ca0892fd8825a4c16a6c6b341c0486adce67e65b
Sebastian Müller <seb.mueller.91@web.de>
2025-02-08 17:45:57 +0100

    Added some implementation

--- checkmatecore/src/main/java/com/dgs/checkmatecore/GameStateProvider.kt
+++ checkmatecore/src/main/java/com/dgs/checkmatecore/GameStateProvider.kt
@@ -5,12 +5,13 @@ import checkmate.model.GameStatus
 import checkmate.model.Move
 import checkmate.model.Position
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 interface GameStateProvider {
-    val gameFlow: Flow<GameState>
-    val canUndoMoveFlow: Flow<Boolean>
-    val canRedoMoveFlow: Flow<Boolean>
-    val gameStatusFlow: Flow<GameStatus>
+    val gameStateFlow: StateFlow<GameState>
+    val canUndoMoveFlow: StateFlow<Boolean>
+    val canRedoMoveFlow: StateFlow<Boolean>
+    val gameStatusFlow: StateFlow<GameStatus>
     fun getMoves(pos: Position): List<Move>
     fun executeMove(move: Move)
 }
--- checkmatecore/src/main/java/com/dgs/checkmatecore/GameStateProviderImpl.kt
+++ checkmatecore/src/main/java/com/dgs/checkmatecore/GameStateProviderImpl.kt
@@ -6,40 +6,51 @@ import checkmate.model.GameState
 import checkmate.model.GameStatus
 import checkmate.model.Move
 import checkmate.model.Position
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
 internal class GameStateProviderImpl @Inject constructor(
-    private val checkmateCore: CheckmateCore
+    private val checkmateCore: CheckmateCore,
+    private val scope: CoroutineScope
 ) : GameStateProvider {
-    private var game: Game = checkmateCore.getInitialGame()
-    private var curGameStateIndex = 0
+    private var _gameFlow = MutableStateFlow(checkmateCore.getInitialGame())
+    private var _curGameStateIndexFlow = MutableStateFlow(0)
 
-    private val _gameFlow = MutableStateFlow(game.gameStates.last())
-    override val gameFlow: Flow<GameState>
-        get() = _gameFlow.asStateFlow().filterNotNull()
+    override val gameStateFlow: StateFlow<GameState> =
+        combine(_gameFlow, _curGameStateIndexFlow) { game, index ->
+            game.gameStates[index]
+        }.stateIn(scope, SharingStarted.Eagerly, _gameFlow.value.gameStates[0])
 
-    override val canUndoMoveFlow: Flow<Boolean> = game.gameStates.
+    override val canUndoMoveFlow: StateFlow<Boolean> =
+        _curGameStateIndexFlow.map { it > 0 }.stateIn(scope, SharingStarted.Eagerly, false)
 
-    override val canRedoMoveFlow: Flow<Boolean>
-        get() = TODO("Not yet implemented")
-    override val gameStatusFlow: Flow<GameStatus>
-        get() = TODO("Not yet implemented")
+    override val canRedoMoveFlow: StateFlow<Boolean> =
+        combine(_gameFlow, _curGameStateIndexFlow) { game, index ->
+            index in 0..<game.gameStates.size - 1
+        }.stateIn(scope, SharingStarted.Eagerly, true)
 
-    init {
-        _gameFlow.update { game.gameStates.last() }
-    }
+    override val gameStatusFlow: StateFlow<GameStatus> =
+        combine(_gameFlow, _curGameStateIndexFlow) { game, index ->
+            game.gameStates[index].gameStatus
+        }.stateIn(scope, SharingStarted.Eagerly, _gameFlow.value.gameStates[0].gameStatus)
 
     override fun getMoves(pos: Position): List<Move> =
-        checkmateCore.getValidMoves(game.gameStates.get(curGameStateIndex), pos)
+        checkmateCore.getValidMoves(
+            _gameFlow.value.gameStates.get(_curGameStateIndexFlow.value),
+            pos
+        )
 
     override fun executeMove(move: Move) {
-        TODO("Not yet implemented")
+        checkmateCore.executeMove(move, _gameFlow.value, _curGameStateIndexFlow.value)
     }
-
 }
